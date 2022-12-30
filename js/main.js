df532402f70521bb28b5e28c677f88270facc485
Yuri Belebekha <ybelebekha@gmail.com>
2022-12-31 00:00:45 +0200

    Abbreviate a Two Word Name

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+// CODEWARS TASK - JS - Abbreviate a Two Word Name
+// https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/train/javascript
+
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+// The output should be two capital letters with a dot separating them.
+// It should look like this:
+// Sam Harris => S.H
+// patrick feeney => P.F
+
+// function abbrevName(name){
+//   let abbreviationArray = [];
+  
+//   name.split(' ').forEach(element => abbreviationArray.push(element.toUpperCase().slice(0, 1)));
+//   console.log(abbreviationArray.join('.'));
+// }
+
+// abbrevName("Sam Harris")      //"S.H"
+// abbrevName("Patrick Feenan")  //"P.F"
+// abbrevName("Evan Cole")       //"E.C"
+// abbrevName("P Favuzzi")       //"P.F"
+// abbrevName("David Mendieta")  //"D.M"
 
 
 
