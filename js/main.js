c57b47320551f8a905235a90cd06e06693b512ee
Yuri Belebekha <ybelebekha@gmail.com>
2023-01-01 15:19:20 +0200

    added solution to Task - Area or Perimeter

--- js/main.js
+++ js/main.js
@@ -12,6 +12,34 @@ function template() {
 
 
 
+// CODEWARS TASK - JS - Area or Perimeter
+// https://www.codewars.com/kata/5ab6538b379d20ad880000ab/train/javascript
+
+// You are given the length and width of a 4-sided polygon.
+// The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle,
+// return its perimeter.
+
+// START OF SOLUTION
+const areaOrPerimeter = function(l , w) {
+  let result = 0;
+
+  if (l === w) {
+    result = l * w;
+  } else {
+    result = l * 2 + w * 2;
+  }  
+  
+  console.log(result);
+};
+
+areaOrPerimeter(3, 3);  // 9
+areaOrPerimeter(6, 10); // 32
+// END OF SOLUTION
+
+
+
+
 // CODEWARS TASK - JS - Double Char
 //https://www.codewars.com/kata/56b1f01c247c01db92000076/train/javascript
 
