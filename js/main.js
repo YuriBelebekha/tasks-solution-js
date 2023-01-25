a318b1206b6035f31b4e2d086b0737ad3e0cb465
Yuri Belebekha <ybelebekha@gmail.com>
2023-01-25 23:38:22 +0200

    Added my solution to Task - Beginner Series #3 Sum of Numbers

--- js/main.js
+++ js/main.js
@@ -12,6 +12,55 @@ function template() {
 
 
 
+// CODEWARS TASK - JS - Beginner Series #3 Sum of Numbers
+// https://www.codewars.com/kata/55f2b110f61eb01779000053/train/javascript
+
+// Given two integers a and b, which can be positive or negative,
+// find the sum of all the integers between and including them and return it.
+// If the two numbers are equal return a or b.
+// Note: a and b are not ordered!
+
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+// START OF SOLUTION
+function getSum(a, b)
+{  
+  let total = 0;
+
+  if (a === b) {
+    console.log(a);
+  };
+
+  if (a < b) {
+    for (let i = a; i <= b; i += 1) {      
+      total += i;
+    }    
+    console.log(total);
+  };
+
+  if (a > b) {
+    for (let i = b; i <= a; i += 1) {      
+      total += i;
+    }    
+    console.log(total);
+  };  
+}
+
+getSum(0, -1); // -1
+getSum(0, 1); //  1
+getSum(2, 2); //  2
+getSum(5, -1); //  14
+getSum(357, -358); //  -358
+
+// END OF SOLUTION
+
+
+
 
 // CODEWARS TASK - JS - Odd or Even?
 // https://www.codewars.com/kata/5949481f86420f59480000e7/train/javascript
