325f22c3487cf68d28cc1ee360110fd40cbc6591
Yuri Belebekha <ybelebekha@gmail.com>
2023-01-27 01:02:19 +0200

    Added my solution to Task - Remove the minimum

--- js/main.js
+++ js/main.js
@@ -10,6 +10,37 @@ function template() {
 //////////////////////////////////////////////////////////////////////
 }
 
+// CODEWARS TASK - JS - Remove the minimum
+// https://www.codewars.com/kata/563cf89eb4747c5fb100001b/train/javascript
+
+// The museum of incredible dull things
+// The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+// However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+
+// Task
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+// Don't change the order of the elements that are left.
+
+// Examples
+// * Input: [1,2,3,4,5], output = [2,3,4,5]
+// * Input: [5,3,2,1,4], output = [5,3,2,4]
+// * Input: [2,2,1,2,1], output = [2,2,2,1]
+
+// START OF SOLUTION
+function removeSmallest(numbers) { 
+  let newArrayNumbers = numbers.slice(0);
+  newArrayNumbers.splice((newArrayNumbers.indexOf(Math.min(...newArrayNumbers))), 1);  
+  console.log(newArrayNumbers);  
+};
+
+removeSmallest([1, 2, 3, 4, 5]); // [2, 3, 4, 5]
+removeSmallest([5, 3, 2, 1, 4]); // [5, 3, 2, 4]
+removeSmallest([2, 2, 1, 2, 1]); // [2, 2, 2, 1]
+removeSmallest([]);              // []
+// END OF SOLUTION
+
+
+
 // CODEWARS TASK - JS - Find the next perfect square!
 // https://www.codewars.com/kata/56269eb78ad2e4ced1000013/train/javascript
 
@@ -27,21 +58,18 @@ function template() {
 
 
 // START OF SOLUTION
-function findNextSquare(sq) {
-  // Return the next square if sq is a perfect square, -1 otherwise
-  
-  const result = Number.isInteger(Math.sqrt(sq)) ? Math.pow((Math.sqrt(sq) + 1), 2) : -1;
-  
+// function findNextSquare(sq) {
+//   // Return the next square if sq is a perfect square, -1 otherwise  
+//   const result = Number.isInteger(Math.sqrt(sq)) ? Math.pow((Math.sqrt(sq) + 1), 2) : -1;
+//   console.log(result);
+//   // return -1;
+// };
 
-    console.log(result);
-  // return -1;
-};
-
-findNextSquare(121);          // 144
-findNextSquare(625);          // 676
-findNextSquare(319225);       // 320356
-findNextSquare(15241383936);  // 15241630849
-findNextSquare(114);  // -1
+// findNextSquare(121);          // 144
+// findNextSquare(625);          // 676
+// findNextSquare(319225);       // 320356
+// findNextSquare(15241383936);  // 15241630849
+// findNextSquare(114);  // -1
 // END OF SOLUTION
 
 
