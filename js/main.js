5b0c30b87e08609cfeaffd3c77afb0b6715c3bf0
Yuri Belebekha <ybelebekha@gmail.com>
2023-01-26 22:25:50 +0200

    Added my solution to Task - Find the next perfect square

--- js/main.js
+++ js/main.js
@@ -10,6 +10,40 @@ function template() {
 //////////////////////////////////////////////////////////////////////
 }
 
+// CODEWARS TASK - JS - Find the next perfect square!
+// https://www.codewars.com/kata/56269eb78ad2e4ced1000013/train/javascript
+
+// You might know some pretty large perfect squares. But what about the NEXT one?
+// Complete the findNextSquare method that finds the next integral perfect square 
+// after the one passed as a parameter.Recall that an integral perfect square is an 
+// integer n such that sqrt(n) is also an integer.
+// If the parameter is itself not a perfect square then - 1 should be returned.
+// You may assume the parameter is non - negative.
+
+// Examples: (Input-- > Output)
+// 121 --> 144
+// 625 --> 676
+// 114 --> -1 since 114 is not a perfect square
+
+
+// START OF SOLUTION
+function findNextSquare(sq) {
+  // Return the next square if sq is a perfect square, -1 otherwise
+  
+  const result = Number.isInteger(Math.sqrt(sq)) ? Math.pow((Math.sqrt(sq) + 1), 2) : -1;
+  
+
+    console.log(result);
+  // return -1;
+};
+
+findNextSquare(121);          // 144
+findNextSquare(625);          // 676
+findNextSquare(319225);       // 320356
+findNextSquare(15241383936);  // 15241630849
+findNextSquare(114);  // -1
+// END OF SOLUTION
+
 
 
 
@@ -25,16 +59,16 @@ function template() {
 // "a234"   -->  false
 
 // START OF SOLUTION
-function validatePIN(pin) {
-  const re = /^(\d{4}|\d{6})$/;
-  const result = re.test(pin);
-  console.log('pin: ', pin, result);  
-}
+// function validatePIN(pin) {
+//   const re = /^(\d{4}|\d{6})$/;
+//   const result = re.test(pin);
+//   console.log('pin: ', pin, result);  
+// }
 
-validatePIN("1234");
-validatePIN("12345");
-validatePIN("123456");
-validatePIN("a234");
+// validatePIN("1234");
+// validatePIN("12345");
+// validatePIN("123456");
+// validatePIN("a234");
 // END OF SOLUTION
 
 
