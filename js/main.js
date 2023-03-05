2fd1d05c632ef216377b3068a64b01a69b37d1f7
Yuri Belebekha <ybelebekha@gmail.com>
2023-03-05 13:45:21 +0200

    Added my solution to Task - Testing 1-2-3

--- js/main.js
+++ js/main.js
@@ -11,6 +11,32 @@ function template() {
 }
 
 
+
+// CODEWARS TASK - JS - Testing 1-2-3
+// https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+// Examples: (Input-- > Output)
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+// START OF SOLUTION
+var number = function (array) {
+  let newArray = [];
+  for (let i = 1; i <= array.length; i += 1) {
+    newArray.push(`${i}: ${array[i-1]}`);
+  }
+  console.log(newArray);
+}
+
+number([]);              // []
+number(["a", "b", "c"]); // ["1: a", "2: b", "3: c"]
+// END OF SOLUTION
+
+//////////////////////////////////////////////////////////////////////
+
+
 // CODEWARS TASK - JS - Are You Playing Banjo?
 // https://www.codewars.com/kata/53af2b8861023f1d88000832/train/javascript
 // Create a function which answers the question "Are you playing banjo?".
