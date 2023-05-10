0af61399cb4b19d9ad3eba0ba5bfb3829d7e336e
Yuri Belebekha <ybelebekha@gmail.com>
2023-05-10 17:55:32 +0300

    Added my solution to Task - Check the exam

--- js/main.js
+++ js/main.js
@@ -11,6 +11,40 @@ function template() {
 };
 
 
+// CODEWARS TASK - JS - Check the exam
+// https://www.codewars.com/kata/5a3dd29055519e23ec000074/train/javascript
+// The first input array is the key to the correct answers to an exam,
+// like["a", "a", "b", "d"].The second one contains a student's submitted answers.
+// The two arrays are not empty and are the same length.Return the score for
+// this array of answers, giving + 4 for each correct answer, -1 for each
+// incorrect answer, and + 0 for each blank answer, represented as an empty
+// string(in C the space character is used).
+// If the score < 0, return 0.
+
+// START OF SOLUTION
+function checkExam(array1, array2) {  
+  let result = 0;
+  
+  for (let i = 0; i < array1.length; i += 1) {
+    !array2[i]
+      ? result += 0 
+      : array1[i] === array2[i]
+        ? result += 4
+        : result -= 1;    
+  };
+  console.log(result < 0 ? 0 : result);
+};
+
+checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]); // 6);
+checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]); // 7);
+checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]); // 16);
+checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]); // 0); 
+// END OF SOLUTION
+
+//////////////////////////////////////////////////////////////////////
+
+
+
 
 // CODEWARS TASK - JS - Remove exclamation marks
 // https://www.codewars.com/kata/57a0885cbb9944e24c00008e/train/javascript
@@ -18,13 +52,13 @@ function template() {
 // from a given string.
 
 // START OF SOLUTION
-function removeExclamationMarks(s) {
-  // console.log(s.split('').map(symbol => symbol.replace(/!/, )));
-  console.log(s.replaceAll(/!/g, ''));
-};
+// function removeExclamationMarks(s) {
+//   // console.log(s.split('').map(symbol => symbol.replace(/!/, )));
+//   console.log(s.replaceAll(/!/g, ''));
+// };
 
-removeExclamationMarks("Hello World!"); // "Hello World"
-removeExclamationMarks("Hello World!!"); // "Hello World"
+// removeExclamationMarks("Hello World!"); // "Hello World"
+// removeExclamationMarks("Hello World!!"); // "Hello World"
 // END OF SOLUTION
 
 //////////////////////////////////////////////////////////////////////
