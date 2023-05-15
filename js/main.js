3cc0ceb2218f9fe3e1bf92363f76e276372ec3ac
Yuri Belebekha <ybelebekha@gmail.com>
2023-05-16 00:00:43 +0300

    Added my solution to Task - Third Angle of a Triangle

--- js/main.js
+++ js/main.js
@@ -11,6 +11,30 @@ function template() {
 };
 
 
+// CODEWARS TASK - JS - Third Angle of a Triangle
+// https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
+// You are given two interior angles (in degrees) of a triangle.
+// Write a function to return the 3rd.
+// Note: only positive integers will be tested.
+// https://en.wikipedia.org/wiki/Triangle
+
+// START OF SOLUTION
+function otherAngle(a, b) {
+  console.log(180 - (a + b);)
+}
+
+otherAngle(30, 60); // 90);
+otherAngle(60, 60); // 60);
+otherAngle(43, 78); // 59);
+otherAngle(10, 20); // 150);
+
+// END OF SOLUTION
+
+//////////////////////////////////////////////////////////////////////
+
+
+
+
 // CODEWARS TASK - JS - Rock Paper Scissors!
 // https://www.codewars.com/kata/5672a98bdbdd995fad00000f/train/javascript
 // Let's play! You have to return which player won! In case of a draw return Draw!.
