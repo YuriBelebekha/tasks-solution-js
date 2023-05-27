d595b41acf491c63612e1a12c756a29606d51917
Yuri Belebekha <ybelebekha@gmail.com>
2023-05-27 21:39:38 +0300

    Added my solution to Task - A wolf in sheep's clothing

--- js/main.js
+++ js/main.js
@@ -11,28 +11,91 @@ function template() {
 };
 
 
+
+
+// CODEWARS TASK - JS - A wolf in sheep's clothing
+// https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15/train/javascript
+// Wolves have been reintroduced to Great Britain.You are a sheep farmer,
+// and are now plagued by wolves which pretend to be sheep.Fortunately,
+// you are good at spotting them.
+
+// Warn the sheep in front of the wolf that it is about to be eaten.
+// Remember that you are standing at the front of the queue which is at
+// the end of the array:
+// [sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+//    7 If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+// Note: there will always be exactly one wolf in the array.
+// Examples
+// Input: ["sheep", "sheep", "sheep", "wolf", "sheep"]
+// Output: "Oi! Sheep number 1! You are about to be eaten by a wolf!"
+// Input: ["sheep", "sheep", "wolf"]
+// Output: "Pls go away and stop eating my sheep"
+
+// START OF SOLUTION
+function warnTheSheep(queue) {
+  const wolfOfIndex = queue.reverse().indexOf('wolf');
+  
+  if (wolfOfIndex > 0) {
+    return console.log(`Oi! Sheep number ${wolfOfIndex}! You are about to be eaten by a wolf!`);
+  } else {
+    console.log(`Pls go away and stop eating my sheep`);
+  };
+  
+  // OR
+
+  // const wolfOfIndex = queue.reverse().indexOf('wolf');
+  
+  // return wolfOfIndex === 0
+  //   ? console.log(`Pls go away and stop eating my sheep`)
+  //   : console.log(`Oi! Sheep number ${wolfOfIndex}! You are about to be eaten by a wolf!`);     
+};
+
+warnTheSheep(["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep"]);
+// "Oi! Sheep number 2! You are about to be eaten by a wolf!"
+
+warnTheSheep(["sheep", "wolf", "sheep", "sheep", "sheep", "sheep", "sheep"]);
+// "Oi! Sheep number 5! You are about to be eaten by a wolf!"
+
+warnTheSheep(["wolf", "sheep", "sheep", "sheep", "sheep", "sheep", "sheep"]);
+// "Oi! Sheep number 6! You are about to be eaten by a wolf!"
+
+warnTheSheep(["sheep", "wolf", "sheep"]);
+// "Oi! Sheep number 1! You are about to be eaten by a wolf!"
+
+warnTheSheep(["wolf"]);
+// "Pls go away and stop eating my sheep"
+    
+warnTheSheep(["sheep", "sheep", "wolf"]);
+// "Pls go away and stop eating my sheep"
+// END OF SOLUTION
+
+//////////////////////////////////////////////////////////////////////
+
+
+
+
 // CODEWARS TASK - JS - Build a pile of Cubes
 // https://www.codewars.com/kata/5592e3bd57b64d00f3000047
 
 // START OF SOLUTION
-function findNb(m) {
-  let sum = 0;
-  let i = 1;
+// function findNb(m) {
+//   let sum = 0;
+//   let i = 1;
 
-  while ( sum < m ) {
-    sum += Math.pow(i, 3);
-    if ( sum === m ) {
-      console.log(i);
-    };
-    i += 1;    
-  };
-  return -1;
-}
+//   while ( sum < m ) {
+//     sum += Math.pow(i, 3);
+//     if ( sum === m ) {
+//       console.log(i);
+//     };
+//     i += 1;    
+//   };
+//   return -1;
+// }
 
-findNb(4183059834009);   // 2022
-findNb(24723578342962);  // -1
-findNb(135440716410000); // 4824
-findNb(40539911473216);  // 3568
+// findNb(4183059834009);   // 2022
+// findNb(24723578342962);  // -1
+// findNb(135440716410000); // 4824
+// findNb(40539911473216);  // 3568
 // END OF SOLUTION
 
 //////////////////////////////////////////////////////////////////////
