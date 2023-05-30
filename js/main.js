6d6b23199f27193e71c9c69e688cb3ded2efcf0e
Yuri Belebekha <ybelebekha@gmail.com>
2023-05-30 19:38:18 +0300

    Added my solution to Task - Find the Remainder

--- js/main.js
+++ js/main.js
@@ -11,6 +11,27 @@ function template() {
 };
 
 
+// CODEWARS TASK - JS - Find the Remainder
+// https://www.codewars.com/kata/524f5125ad9c12894e00003f/train/javascript
+// Write a function that accepts two integers and returns the remainder of
+// dividing the larger value by the smaller value.
+// Division by zero should return NaN.
+
+// START OF SOLUTION
+function remainder(n, m) {
+  console.log(n > m ? n % m : m % n)
+};
+
+remainder(17, 5);  // 2, 'Returned value should be the value left over after dividing as much as possible. For input n = 17, m = 5');
+remainder(13, 72); // 7 remainder(72, 13), 'The order the arguments are passed should not matter');
+remainder(1, 0);   // 'Divide by zero should return NaN');
+remainder(0, 0);   // 'Divide by zero should return NaN');
+// END OF SOLUTION
+
+//////////////////////////////////////////////////////////////////////
+
+
+
 
 // CODEWARS TASK - JS - Hello, Name or World!
 // https://www.codewars.com/kata/57e3f79c9cb119374600046b/train/javascript
@@ -20,21 +41,21 @@ function template() {
 // a name with a first capital letter(Xxxx).
 
 // START OF SOLUTION
-function hello(name) {
-  if (name) {
-    const normalizedString = name.toLowerCase();
-    const firstLetter = normalizedString.charAt(0).toUpperCase();
-    const nameWithoutFirstLetter = normalizedString.slice(1);
+// function hello(name) {
+//   if (name) {
+//     const normalizedString = name.toLowerCase();
+//     const firstLetter = normalizedString.charAt(0).toUpperCase();
+//     const nameWithoutFirstLetter = normalizedString.slice(1);
 
-    console.log(`Hello, ${firstLetter.concat(nameWithoutFirstLetter)}!`);
-  } else {
-    console.log(`Hello, World!`);
-  };
-}
+//     console.log(`Hello, ${firstLetter.concat(nameWithoutFirstLetter)}!`);
+//   } else {
+//     console.log(`Hello, World!`);
+//   };
+// }
 
-hello('alice'); // 'Hello, Alice!', "returns 'Hello, Alice!' when given 'alice'");
-hello();        // 'Hello, World!', "returns 'Hello, World!' when name is not given");
-hello('');      // 'Hello, World!', "returns 'Hello, World!' when name is an empty string");
+// hello('alice'); // 'Hello, Alice!', "returns 'Hello, Alice!' when given 'alice'");
+// hello();        // 'Hello, World!', "returns 'Hello, World!' when name is not given");
+// hello('');      // 'Hello, World!', "returns 'Hello, World!' when name is an empty string");
 // END OF SOLUTION
 
 //////////////////////////////////////////////////////////////////////
