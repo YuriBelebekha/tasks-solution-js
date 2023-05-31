35960d9984b8de2a15ab54638b87825001c2af4f
Yuri Belebekha <ybelebekha@gmail.com>
2023-05-31 23:52:07 +0300

    Added my solution to Task - Grasshopper - Grade book

--- js/main.js
+++ js/main.js
@@ -11,6 +11,70 @@ function template() {
 };
 
 
+
+// CODEWARS TASK - JS - Grasshopper - Grade book
+// https://www.codewars.com/kata/55cbd4ba903825f7970000f5/train/javascript
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
+// START OF SOLUTION
+function getGrade (s1, s2, s3) {
+  const averageScore = Math.round((s1 + s2 + s3) / 3);  
+
+  if (averageScore >= 90) {
+    console.log('A');
+  };
+
+  if (averageScore >= 80 && averageScore < 90) {
+    console.log('B');
+  };
+
+  if (averageScore >= 70 && averageScore < 80) {
+    console.log('C');
+  };
+
+  if (averageScore >= 60 && averageScore < 70) {
+    console.log('D');
+  };
+
+  if (averageScore > 0 && averageScore < 60) {
+    console.log('F');
+  };
+
+  if (!averageScore) {
+    console.log('F');
+  };
+};
+
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+
+getGrade(95, 90, 93);  // 'A'
+getGrade(100, 85, 96); // 'A' 
+getGrade(92, 93, 94);  // 'A'
+getGrade(70, 70, 100); // 'B')
+getGrade(82, 85, 87);  // 'B')
+getGrade(84, 79, 85);  // 'B')
+getGrade(89, 89, 90);  // 'B')
+getGrade(70, 70, 70);  // 'C')
+getGrade(75, 70, 79);  // 'C')
+getGrade(60, 82, 76);  // 'C')
+getGrade(65, 70, 59);  // 'D')
+getGrade(66, 62, 68);  // 'D')
+getGrade(58, 62, 70);  // 'D')
+getGrade(44, 55, 52);  // 'F')
+getGrade(48, 55, 52);  // 'F')
+getGrade(58, 59, 60);  // 'F')
+// END OF SOLUTION
+
+//////////////////////////////////////////////////////////////////////
+
+
+
+
 // CODEWARS TASK - JS - Find the Remainder
 // https://www.codewars.com/kata/524f5125ad9c12894e00003f/train/javascript
 // Write a function that accepts two integers and returns the remainder of
@@ -18,14 +82,14 @@ function template() {
 // Division by zero should return NaN.
 
 // START OF SOLUTION
-function remainder(n, m) {
-  console.log(n > m ? n % m : m % n)
-};
+// function remainder(n, m) {
+//   console.log(n > m ? n % m : m % n)
+// };
 
-remainder(17, 5);  // 2, 'Returned value should be the value left over after dividing as much as possible. For input n = 17, m = 5');
-remainder(13, 72); // 7 remainder(72, 13), 'The order the arguments are passed should not matter');
-remainder(1, 0);   // 'Divide by zero should return NaN');
-remainder(0, 0);   // 'Divide by zero should return NaN');
+// remainder(17, 5);  // 2, 'Returned value should be the value left over after dividing as much as possible. For input n = 17, m = 5');
+// remainder(13, 72); // 7 remainder(72, 13), 'The order the arguments are passed should not matter');
+// remainder(1, 0);   // 'Divide by zero should return NaN');
+// remainder(0, 0);   // 'Divide by zero should return NaN');
 // END OF SOLUTION
 
 //////////////////////////////////////////////////////////////////////
