dbda92aa36a3050fa26a7dff09b026b94612197a
Yuri Belebekha <ybelebekha@gmail.com>
2023-06-25 14:17:43 +0300

    Added my solution to Task - What's the real floor?

--- js/main.js
+++ js/main.js
@@ -11,6 +11,41 @@ function template() {
 };
 
 
+// CODEWARS TASK - JS - What's the real floor?
+// https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
+// Americans are odd people: in their buildings, the first floor is actually the ground
+// floor and there is no 13th floor(due to superstition).
+
+// Write a function that given a floor in the american system returns the floor in the european system.
+
+// With the 1st floor being replaced by the ground floor and the 13th floor being removed, the numbers
+// move down to take their place.In case of above 13, they move down by two because there are two omitted
+// numbers below them.
+
+// Basements (negatives) stay the same as the universal level.
+
+// START OF SOLUTION
+function getRealFloor(n) {
+  const result =
+    n <= 0
+      ? n
+      : n >= 13
+        ? n - 2
+        : n - 1;
+  console.log(result);
+};
+
+getRealFloor( 1);  //  0
+getRealFloor( 5);  //  4
+getRealFloor(15);  // 13
+getRealFloor(-2);  // -2
+// END OF SOLUTION
+
+//////////////////////////////////////////////////////////////////////
+
+
+
+
 // CODEWARS TASK - JS - Printing Array elements with Comma delimiters
 // https://www.codewars.com/kata/56e2f59fb2ed128081001328/train/javascript
 // Input: Array of elements
@@ -20,12 +55,12 @@ function template() {
 // "h,o,l,a"
 
 // START OF SOLUTION
-function printArray(array){
-  console.log(array.toString())
-};
+// function printArray(array){
+//   console.log(array.toString())
+// };
 
-printArray([2, 4, 5, 2]);      // "2,4,5,2"
-printArray(["h","o","l","a"]); // "h,o,l,a"
+// printArray([2, 4, 5, 2]);      // "2,4,5,2"
+// printArray(["h","o","l","a"]); // "h,o,l,a"
 // END OF SOLUTION
 
 //////////////////////////////////////////////////////////////////////
